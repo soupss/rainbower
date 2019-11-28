4e90f1c0ec391a0b61af1d9b69fe895af12fb64c
Anton Laurell <antonlaurell@hotmail.com>
2019-11-28 15:15:08 +0100

    Sort by hue

--- rainbower/script.py
+++ rainbower/script.py
@@ -2,7 +2,7 @@
 Rainbower.
 
 Desc:
-generates a rainbow
+generates N colors and sorts by hue, resulting in a random rainbow.
 
 Tools:
 Pygame
@@ -17,21 +17,37 @@ class Bit:
         self.color = color
         self.rect = rect
 
+    def draw(self, surface):
+        '''Draw bit to the specified surface'''
+        pygame.draw.rect(surface, self.color, self.rect)
+
+    def log(self, index):
+        '''Log information about bit'''
+        print('\nBit n' + str(index + 1))
+        print('rgb: ' + str(self.color.r), str(self.color.g), str(self.color.b))
+        print('pos: ' + str(self.rect.x))
+        print('hue: ' + str(self.color.hsva[0]))
+
+
+def hue(elem):
+    '''Get hue of element with color object'''
+    return elem.color.hsva[0]
+
 
 def setup():
     '''Setup program variables'''
     pygame.init()
 
-    window = pygame.display.set_mode((800, 200))
+    window = pygame.display.set_mode((1000, 200))
 
     bits = []
-    max_bits = 20
+    max_bits = 25 # window.get_width() % max_bit equal to 0 for best performance
 
     # fill array with bits
     while len(bits) < max_bits:
         width = window.get_width() / max_bits
         height = window.get_height()
-        x = len(bits) * width
+        x = 0
         y = 0
         r = randint(0, 255)
         g = randint(0, 255)
@@ -43,15 +59,15 @@ def setup():
         bit = Bit(color, rect)
         bits.append(bit)
 
-    # draw colors
-    for bit in bits:
-        pygame.draw.rect(window, bit.color, bit.rect)
+    # sort bits in array
+    bits.sort(key=hue)
+    for index, bit in enumerate(bits):
+        # sort bits visually
+        bit.rect.x = index * bit.rect.width
 
-        print('\nBit index ' + str(bits.index(bit)))
-        print(' color: ' + str(bit.color))
-        print(' rect: ' + str(bit.rect))
-
-    print('\nTotal n of bits = ' + str(len(bits)))
+        #draw bits
+        bit.draw(window)
+        bit.log(index)
 
 
 def update():
