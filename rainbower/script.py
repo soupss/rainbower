fdc1f866c9ce0ef637985ebf2bada8842f9a4af6
Anton Laurell <antonlaurell@hotmail.com>
2019-11-28 13:25:32 +0100

    Changed arguments for class 'Bit'

--- rainbower/script.py
+++ rainbower/script.py
@@ -12,56 +12,50 @@ import pygame
 
 
 class Bit:
-    def __init__(self, x, y, width, height, r, g, b):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.r = r
-        self.g = g
-        self.b = b
-
-        self.color = pygame.Color(self.r, self.g, self.b)
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+    '''Bit of color'''
+    def __init__(self, color, rect):
+        self.color = color
+        self.rect = rect
 
 
 def setup():
+    '''Setup program variables'''
     pygame.init()
 
-    WINDOW = pygame.display.set_mode((800, 200))
+    window = pygame.display.set_mode((800, 200))
 
     bits = []
-    maxBits = 20
+    max_bits = 20
 
     # fill array with bits
-    while len(bits) < maxBits:
-        width = WINDOW.get_width() / maxBits
-        height = WINDOW.get_height()
+    while len(bits) < max_bits:
+        width = window.get_width() / max_bits
+        height = window.get_height()
         x = len(bits) * width
         y = 0
         r = randint(0, 255)
         g = randint(0, 255)
         b = randint(0, 255)
 
-        bit = Bit(x, y, width, height, r, g, b)
+        color = pygame.Color(r, g, b)
+        rect = pygame.Rect(x, y, width, height)
+
+        bit = Bit(color, rect)
         bits.append(bit)
 
     # draw colors
     for bit in bits:
-        pygame.draw.rect(WINDOW, bit.color, bit.rect)
+        pygame.draw.rect(window, bit.color, bit.rect)
 
         print('\nBit index ' + str(bits.index(bit)))
-        print('    x: ' + str(bit.x))
-        print('    y: ' + str(bit.y))
-        print('    width: ' + str(bit.width))
-        print('    height: ' + str(bit.height))
-        print('    rgb: ' + str(bit.r), str(bit.g), str(bit.b))
+        print(' color: ' + str(bit.color))
+        print(' rect: ' + str(bit.rect))
 
     print('\nTotal n of bits = ' + str(len(bits)))
 
 
 def update():
-    '''update'''
+    '''Update program variables'''
     run = True
     while run:
         pygame.time.delay(100)
@@ -71,6 +65,3 @@ def update():
                 run = False
 
         pygame.display.update()
-
-def draw():
-    '''draw'''
